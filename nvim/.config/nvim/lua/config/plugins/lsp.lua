b0857cc45f5729efc98e2c34bdea9babdcfbe24e
dotjberg <goldbergjarrett@gmail.com>
2025-11-04 22:25:34 -0500

    Fix LSP and add diagnostics.

--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -1,32 +1,56 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "harper_ls", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "harper_ls", "gopls", "zls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
     dependencies = {
-      'saghen/blink.cmp',
+      "saghen/blink.cmp",
     },
-		config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.gopls.setup({  capabilities = capabilities })
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+      })
+
+      -- Diagnostic signs
+      local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "»" }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
+
+      -- Enable LSP servers
+      vim.lsp.enable({ "lua_ls", "gopls", "zls", "harper_ls" })
+
+      -- LSP keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+      -- Diagnostic keymaps
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
+    end,
+  },
 }
